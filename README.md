2edd8f4f32a3454ba521c773c8e519e8409bb4e7
boytjuge <ponlawat007@gmail.com>
2025-01-15 00:36:17 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+"# ollama-chatbot-api" 
+
+
+# flask python 3.11.1
+# ollama 3.2
+# cpu i7 gen4
+# window 10
---
+++ testollama.py
@@ -0,0 +1,45 @@
+from flask import Flask , render_template , url_for , jsonify ,redirect ,session,request , Response
+import   ollama
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'fafwfwfwr25*cmcXdm1!2421'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+desiredModel = 'llama3.2:latest'
+questiontToAsk = "what is youtube ?"
+
+
+# response = await ollama.chat(model=desiredModel, messages =[{
+#         'role':'user',
+#         'content':questiontext,
+#     },])
+# OllamaResponse = response['message']['content']
+# print(OllamaResponse)
+
+
+@app.route('/chatapi', methods=['POST'])
+def  chatapi():
+    recieve = request.json
+    questiontext = recieve['question']
+    def generate():
+        response = ollama.chat(model=desiredModel, messages=[{
+            'role': 'user',
+            'content': questiontext,
+        }])
+
+        OllamaResponse = response['message']['content']
+
+        # Split response by sentence or other logic
+        sentences = OllamaResponse.split('. ')
+        for sentence in sentences:
+            yield sentence + '. '  # Yielding each sentence as it's processed
+
+    return Response(generate(), content_type='text/plain; charset=utf-8')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1',port="7778")
